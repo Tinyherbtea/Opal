7d0daa37e62ce255f91016659ce5bbf8c664dfbd
AmolassDBW <1942591443@qq.com>
2026-05-11 18:56:47 +0800

    Add premake lua file for vs

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Opal"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir ="%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Opal"
+	location "Opal"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/"..outputdir .."/%{prj.name}")
+	objdir ("bin-intermediate/"..outputdir .."/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	includedirs
+	{
+
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.26100.0"
+
+	defines
+	{
+		"OPALLOG_EXPORTS",
+		"OPAL_PLATFORM_WINDOWS",
+		"OPAL_BUILD_DLL"
+	}
+	
+	postbuildcommands
+	{
+		("xcopy /Q /E /Y /I ..\\bin\\" .. outputdir .. "\\Opal\\*.dll ..\\bin\\" .. outputdir .. "\\Sandbox > nul")
+	}
+
+	filter "configurations:Debug"
+		defines "OPAL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OPAL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OPAL_DIST"
+		optimize "On"
+
+	
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("bin/"..outputdir .."/%{prj.name}")
+	objdir ("bin-intermediate/"..outputdir .."/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	includedirs
+	{
+		"Opal/src"
+	}
+
+	links
+	{
+		"Opal"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.26100.0"
+
+	defines
+	{
+		"OPAL_PLATFORM_WINDOWS"
+	}
+
+	filter "configurations:Debug"
+		defines "OPAL_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OPAL_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OPAL_DIST"
+		optimize "On"
